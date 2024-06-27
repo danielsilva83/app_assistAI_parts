1cdb229eb9419c0bddd8d52f37b3e575da832608
marcellaresende <marcella_peralta14@hotmail.com>
2024-06-27 14:25:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,10 +31,14 @@ examples_df = pd.read_csv(examples_csv_path)
 
 # Carregar o modelo
 model1 = load_model('./static/modelo_spacy4.h5')
-def load_examples(start=0, count=50):
+
+def load_examples_ini(start=150, count=10):
     print( examples_df.iloc[start:start + count].to_dict(orient='records'))
     return examples_df.iloc[start:start + count].to_dict(orient='records')
 
+def load_examples(n=7):
+    df = pd.read_csv(examples_csv_path)
+    return df.sample(n).to_dict(orient='records')
 
 def preprocess_text(text):
     doc = nlp(text)
@@ -55,7 +59,7 @@ def predict(df, model, text_columns, scaler, encoder, code_to_description, top_k
     X_processed = preprocess_input(df, text_columns, scaler)
     predictions = model.predict(X_processed)
     top_k_indices = np.argsort(predictions, axis=1)[:, -top_k:][:, ::-1]
-    top_k_probabilities = np.sort(predictions, axis=1)[:, -top_k:][:, ::-1]
+    top_k_probabilities = np.sort(predictions, axis=1)[:, -top_k:][:, ::-1] * 100 
     top_k_labels = [encoder.inverse_transform(indices) for indices in top_k_indices]
     top_k_descriptions = [[code_to_description[label] for label in labels] for labels in top_k_labels]
     return top_k_labels, top_k_probabilities, top_k_descriptions
@@ -63,7 +67,7 @@ def predict(df, model, text_columns, scaler, encoder, code_to_description, top_k
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    examples = load_examples()
+    examples = load_examples_ini()
     
     if request.method == 'POST':
         modelo = request.form['modelo']
